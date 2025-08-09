3a2e3f38d8ab822ebda84dabd2766be1aeb31b6b
Daksh Yadav <dakshdav@gmail.com>
2025-08-10 00:23:54 +0530

    created the api endpoints and included tanstack query and devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import { ThemeProvider } from "./Context/ThemeProvider";
-import WeatherDashboard from "./pages/weatherDashboard";
-import CityPage from "./pages/cityPage";
+import WeatherDashboard from "./pages/WeatherDashboard";
+import CityPage from "./pages/CityPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <div>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </div>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 };
 
